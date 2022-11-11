2f530f76fe5a300e59fa4d8f4766a7bd727a1b1c
sofie-ventzel <sofieventzel@hotmail.com>
2022-11-11 16:15:26 +0000

    added js file for challenge code with pseudocode

---
+++ 04-console-app-lesson/challenge/starter/my_file.js
@@ -0,0 +1,7 @@
+// print every answear to the console
+// Sum the total amount of months in the array
+// Total the array, aka the net total amount of Profit/Losses
+// Track the changes from month to month
+// Find the average of the changes, perhaps a new array to track the changes?
+// Find the greatest increase in profits (date and amount)
+// Find the greatest decrease in losses (date and amount)
