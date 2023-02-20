4b549f3c77c86e42a9ec902f2f97bcc5ff0cf484
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-20 10:10:23 +0000

    tried to total array with function, not working yet

--- my_file.js
+++ my_file.js
@@ -1,14 +1,23 @@
-
-
 // print every answear to the console
-for (Object of finances) {
-    console.log(Object)
+for ( i=0; i<finances.length; i++) {
+    console.log('All the profits and losses: ' + finances)
 }
 
 // Sum the total amount of months in the array
 console.log('Total number of months: ' + finances.length);
 
 // Total the array, aka the net total amount of Profit/Losses
+function sumFinances(finances) {
+  var sum = 0;
+    for ( i=0; i<finances.length; i++) {
+        sum += finances[i]    
+    };
+    console.log('Total: ' + sum); 
+    return sum
+}
+
+sumFinances(finances[i][1])
+
 
 // Track the changes from month to month
 // Find the average of the changes, perhaps a new array to track the changes?
