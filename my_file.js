d234c737d112f8214262cde60659da78930356a4
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-20 10:30:48 +0000

    made sum function work with a parenthesis

--- my_file.js
+++ my_file.js
@@ -10,16 +10,19 @@ console.log('Total number of months: ' + finances.length);
 function sumFinances(finances) {
   var sum = 0;
     for ( i=0; i<finances.length; i++) {
-        sum += finances[i]    
+        sum += (finances[i][1])    
     };
     console.log('Total: ' + sum); 
     return sum
 }
 
-sumFinances(finances[i][1])
+sumFinances(finances)
 
 
 // Track the changes from month to month
+for ( i=0; i<finances.length; i++) {
+    console.log(finances[i][1])
+}
 // Find the average of the changes, perhaps a new array to track the changes?
 // Find the greatest increase in profits (date and amount)
 // Find the greatest decrease in losses (date and amount)
