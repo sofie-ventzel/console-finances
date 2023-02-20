9cbf59487c27f7c10ffa555df5f18f29d0e90ff6
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-20 11:57:10 +0000

    attempt to find greatest increase in profit, not there yet

--- my_file.js
+++ my_file.js
@@ -7,7 +7,7 @@ for ( i=0; i<finances.length; i++) {
 console.log('Total number of months: ' + finances.length);
 
 // Total the array, aka the net total amount of Profit/Losses
-function sumFinances(finances) {
+function sumFinances() {
   var sum = 0;
     for ( i=0; i<finances.length; i++) {
         sum += (finances[i][1])    
@@ -20,9 +20,32 @@ sumFinances(finances)
 
 
 // Track the changes from month to month
-for ( i=0; i<finances.length; i++) {
-    console.log(finances[i][1])
+function changes() {
+    for ( i=0; i<finances.length; i++) {
+        
+        
+    } 
+    console.log('something')
+    return; 
 }
+
+changes(finances)
+
 // Find the average of the changes, perhaps a new array to track the changes?
+
+
 // Find the greatest increase in profits (date and amount)
+// if the index number is higher, compare to the next one
+function greatestIncrease() {
+    var sum = 0;
+    for ( i=0; i<finances.length; i++) {
+        if ((sum += (finances[i][1])) < (finances[i++][1]) => (sum === )) {
+            sum = (finances[i++][1])
+        }     
+    };
+    console.log('Greatest increase in profits: ' + sum ); 
+    return; 
+}
+
+greatestIncrease(finances)
 // Find the greatest decrease in losses (date and amount)
